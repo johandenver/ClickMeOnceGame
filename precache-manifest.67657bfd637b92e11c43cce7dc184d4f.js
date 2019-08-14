8f851566ff1edb29a13b02a4b40caad3acf17560
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-08-13 20:12:17 -0700

    Updates

---
+++ precache-manifest.67657bfd637b92e11c43cce7dc184d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c316273e0f55fe229036b176c0faf7d",
+    "url": "/ClickMeOnceGame/index.html"
+  },
+  {
+    "revision": "24770362565f504dff57",
+    "url": "/ClickMeOnceGame/static/css/main.b3991686.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/ClickMeOnceGame/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "24770362565f504dff57",
+    "url": "/ClickMeOnceGame/static/js/main.64872014.chunk.js"
+  },
+  {
+    "revision": "48da2f52b25c0af60d5e",
+    "url": "/ClickMeOnceGame/static/js/runtime~main.eedeb242.js"
+  },
+  {
+    "revision": "79271f16dd1ebef5b4538e80d1ffd3f6",
+    "url": "/ClickMeOnceGame/static/media/night-sky.79271f16.jpg"
+  }
+]);
